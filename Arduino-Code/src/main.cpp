a2c7e1cbb945e82dcbeb1f490cc640eb4e6b2947
rubikscraft <renzodepenzo@live.nl>
2020-09-18 00:06:16 +0200

    add some decoration

--- Arduino-Code/src/main.cpp
+++ Arduino-Code/src/main.cpp
@@ -6,13 +6,24 @@
 #define PIN_MOTOR_PWM_RIGHT 5
 #define PIN_MOTOR_PWM_LEFT 6
 
+#define MIN_DISTANCE 15
+
 #define I2C_ADDRESS 0x20
 #define LEDS_COUNT 10
 
+#define PIN_SONIC_TRIG 7
+#define PIN_SONIC_ECHO 8
+#define MAX_DISTANCE 100
+#define SONIC_TIMEOUT (MAX_DISTANCE * 60)
+#define SOUND_VELOCITY 340
+
 Freenove_WS2812B_Controller strip(I2C_ADDRESS, LEDS_COUNT, TYPE_GRB);
+float currentDistance = MAX_DISTANCE;
+
+int currentLeftSpeed = 0;
+int currentRightSpeed = 0;
 
 void setup()
-
 {
   Serial.begin(9600);
   while (!strip.begin())
@@ -23,12 +34,34 @@ void setup()
   pinMode(PIN_DIRECTION_RIGHT, OUTPUT);
   pinMode(PIN_MOTOR_PWM_RIGHT, OUTPUT);
 
-  strip.setAllLedsColor(255, 255, 255);
-  strip.show();
+  pinMode(PIN_SONIC_TRIG, OUTPUT);
+  pinMode(PIN_SONIC_ECHO, INPUT);
 }
 
-void motor(int pwmspeed, int motor)
+float getSonar()
 {
+  unsigned long pingTime;
+  float distance;
+
+  digitalWrite(PIN_SONIC_TRIG, HIGH); // make trigPin output high level lasting for 10μs to triger HC_SR04,
+  delayMicroseconds(10);
+  digitalWrite(PIN_SONIC_TRIG, LOW);
+
+  pingTime = pulseIn(PIN_SONIC_ECHO, HIGH, SONIC_TIMEOUT); // Wait HC-SR04 returning to the high level and measure out this waitting time
+  if (pingTime != 0)
+    distance = (float)pingTime * SOUND_VELOCITY / 2 / 10000; // calculate the distance according to the time
+  else
+    distance = MAX_DISTANCE;
+  return distance; // return the distance value
+}
+
+void motor(int pwmspeed, int motor, bool block = false)
+{
+  if (pwmspeed > 0 && block)
+  {
+    pwmspeed = 0;
+  }
+
   bool dir = pwmspeed < 0;
   int motorSpeed = abs(pwmspeed);
 
@@ -53,29 +86,49 @@ void readCommand(String command)
 {
   char base = command.charAt(0);
 
-  int leftspeed = 0;
-  int rightspeed = 0;
-
   if (base == 'L')
   {
-    leftspeed = command.substring(2).toInt() * negative(command.charAt(1));
+    currentLeftSpeed = command.substring(2).toInt() * negative(command.charAt(1));
   }
   else if (base == 'R')
   {
-    rightspeed = command.substring(2).toInt() * negative(command.charAt(1));
+    currentRightSpeed = command.substring(2).toInt() * negative(command.charAt(1));
   }
   else if (base == 'A')
   {
     int action = String(command.charAt(1)).toInt();
+    switch (action)
+    {
+    default:
+      strip.setAllLedsColor(255, 255, 255);
+      strip.show();
+      delay(100);
+      break;
+    }
     // Do actions
   }
-
-  motor(leftspeed, 0);
-  motor(rightspeed, 1);
 }
 
 void loop()
 {
-  String command = Serial.readStringUntil('\n');
-  readCommand(command);
+  if (Serial.available())
+  {
+    String command = Serial.readStringUntil('\n');
+
+    readCommand(command);
+  }
+
+  currentDistance = getSonar();
+
+  bool block = currentDistance < MIN_DISTANCE;
+
+  motor(currentLeftSpeed, 0, block);
+  motor(currentRightSpeed, 1, block);
+
+  if (block)
+    strip.setAllLedsColor(10, 0, 0);
+  else
+    strip.setAllLedsColor(0, 10, 0);
+
+  strip.show();
 }
